81d58980244af5e320a7438f81b5a082ea0a4cbc
Andrew Speth <andrew.speth@locushealth.com>
2024-06-28 20:06:19 -0600

    add custom pastel colors to indent-rainbowline

--- lua/plugins/indent-rainbowline.lua
+++ lua/plugins/indent-rainbowline.lua
@@ -2,7 +2,16 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   opts = function(_, opts)
     -- Other blankline configuration here
-    return require("indent-rainbowline").make_opts(opts)
+    return require("indent-rainbowline").make_opts(opts, {
+      -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
+      color_transparency = 0.18,
+      colors = {
+        0xFFFFBA, -- Pastel Yellow
+        0xBAFFC9, -- Pastel Green
+        0xFFB3FF, -- Pastel Pink/Magenta
+        0xB3FFFF, -- Pastel Cyan
+      },
+    })
   end,
   dependencies = {
     "TheGLander/indent-rainbowline.nvim",
