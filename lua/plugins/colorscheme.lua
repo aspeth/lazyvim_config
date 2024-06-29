c89a9047c37cdfd18a410060d72887d6c5300bbe
Andrew Speth <andrew.speth@locushealth.com>
2024-06-28 20:05:53 -0600

    switch to tokyo night transparent colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-  { "oxfist/night-owl.nvim", lazy = false, opts = { transparent_background = true } },
-  -- { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = { transparent = true } },
-  -- { "catppuccin/nvim", name = "catppuccin", opts = { transparent_background = true } },
-  -- { "rose-pine/neovim", name = "rose-pine", opts = { styles = { transparency = true } } },
-  -- { "EdenEast/nightfox.nvim", opts = { transparent = true } },
+  -- { "oxfist/night-owl.nvim", lazy = false, opts = { transparent_background = true } },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = { transparent = true } },
+  -- { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  -- -- { "catppuccin/nvim", name = "catppuccin", opts = { transparent_background = true } },
+  -- -- { "rose-pine/neovim", name = "rose-pine", opts = { styles = { transparency = true } } },
+  -- -- { "EdenEast/nightfox.nvim", opts = { transparent = true } },
   -- {
   --   "eldritch-theme/eldritch.nvim",
   --   lazy = false,
@@ -13,7 +14,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "night-owl",
+      colorscheme = "tokyonight",
     },
   },
 }
