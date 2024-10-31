cb39df8d34c4ae5f1a3494c44f905d8d344b2743
Andrew Speth <andrew.speth@locushealth.com>
2024-10-31 14:45:26 -0600

    try out ruby_lsp instead of solargraph

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -23,24 +23,28 @@ return {
           cmd = { "bundle", "exec", "rubocop", "--lsp" },
           root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         },
-        sqlls = {},
-        solargraph = {
-          -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
-          cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
+        ruby_lsp = {
+          cmd = { os.getenv("HOME") .. "/.asdf/shims/ruby-lsp", "stdio" },
           root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-          settings = {
-            solargraph = {
-              autoformat = true,
-              completion = true,
-              diagnostics = false,
-              folding = true,
-              references = true,
-              rename = true,
-              symbols = true,
-            },
-          },
         },
-        tsserver = {},
+        sqlls = {},
+        -- solargraph = {
+        --   -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
+        --   cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
+        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        --   settings = {
+        --     solargraph = {
+        --       autoformat = true,
+        --       completion = true,
+        --       diagnostics = false,
+        --       folding = true,
+        --       references = true,
+        --       rename = true,
+        --       symbols = true,
+        --     },
+        --   },
+        -- },
+        ts_ls = {},
         yamlls = {},
       },
     },
